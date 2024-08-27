7e72001ea00791cca9c5c3cda6376a120e3eb8e8
Roman.Hanmamedov <Roman.Hanmamedov@EST-111.EST.local>
2024-08-27 13:47:49 +0300

    Revert "Added HttpUtils class, to be able handle HTTP GET requests."
    This reverts commit cf431b3320d5fc5f8a83ae8670bf10ce35104db7.

--- src/main/java/eu/romanhan/youtube/HttpUtils.java
+++ src/main/java/eu/romanhan/youtube/HttpUtils.java
@@ -1,26 +1,13 @@
 package eu.romanhan.youtube;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-public class HttpUtils {
-
-	public static String sendGetRequest(String urlString) throws IOException {
-		URL url = new URL(urlString);
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestMethod("GET");
-		StringBuilder response = new StringBuilder();
-
-		try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-			String inputLine;
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-		}
-		return response.toString();
-	}
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
