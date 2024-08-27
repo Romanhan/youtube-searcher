24a32c4d3019f57bcd3068fd24df56707fefc41e
Roman.Hanmamedov <Roman.Hanmamedov@EST-111.EST.local>
2024-08-27 12:51:38 +0300

    Initial commit.

---
+++ src/main/java/eu/romanhan/youtube/App.java
@@ -0,0 +1,13 @@
+package eu.romanhan.youtube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/eu/romanhan/youtube/YoutubeSearcher.java
@@ -0,0 +1,8 @@
+package eu.romanhan.youtube;
+
+public class YoutubeSearcher {
+
+	private static final String API_KEY = "AILI";
+	private static final String API_IRL = "https://www.googleapis.com/youtube/v3/search";
+
+}
